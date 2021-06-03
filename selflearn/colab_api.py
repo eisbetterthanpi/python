8be5ce3e25591a82d74d463cae73b232a4360f67
eisbetterthanpi <60023474+eisbetterthanpi@users.noreply.github.com>
2021-06-03 16:07:59 +0800

    Add files via upload

--- selflearn/colab_api.py
+++ selflearn/colab_api.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium_stealth import stealth
 import time
 
-
 # https://stackoverflow.com/questions/66209119/automation-google-login-with-python-and-selenium-shows-this-browser-or-app-may
 options = webdriver.ChromeOptions()
 # WINDOW_SIZE = "1280,720"
@@ -34,12 +33,13 @@ stealth(driver,
     )
 
 driver.get('https://colab.research.google.com/drive/1GPgQZGxFgQp9vJbaLJTp9rB-PEhECbXO')
-# driver.get('https://colab.research.google.com/drive/1GPgQZGxFgQp9vJbaLJTp9rB-PEhECbXO?usp=sharing')
-# driver.get('https://accounts.google.com/signin/v2/identifier?passive=true&continue=https%3A%2F%2Fcolab.research.google.com%2Fdrive%2F1GPgQZGxFgQp9vJbaLJTp9rB-PEhECbXO&ec=GAZAqQM&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
-# https://accounts.google.com/signin/v2/identifier?passive=true&continue=https%3A%2F%2Fcolab.research.google.com%2Fdrive%2F1GPgQZGxFgQp9vJbaLJTp9rB-PEhECbXO&ec=GAZAqQM&flowName=GlifWebSignIn&flowEntry=ServiceLogin
 
+# time.sleep(3)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+myElem = WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.ID, ':1x')))
 
-time.sleep(3)
 
 print(driver.title)
 # ctrl-F9
@@ -48,52 +48,71 @@ print(driver.title)
 setup='''
 document.getElementsByTagName("colab-run-button")[0].click()
 '''
-driver.execute_script(setup)
+# driver.execute_script(setup)
 # document.getElementById("identifierNext").click()
 
-time.sleep(3)
-
-print(driver.title)
-# pop up not authored by google
-popup='''
-document.getElementById("ok").click()
-'''
-# driver.execute_script(popup)
+# time.sleep(3)
 
 
-time.sleep(3)
+# if EC.presence_of_element_located((By.ID, ':1x'): # pop up not authored by google
+# driver.execute_script("document.getElementById('ok').click()")
+# ok=driver.find_element_by_xpath("//div[@id='ok']")
 
+# <colab-dialog class="yes-no-dialog"
+# <paper-dialog role="dialog"
+# #shadow-root (open)
+# usage limits usage-limit
+
+
+# time.sleep(3)
+import sys
 while True:
     print(driver.title)
     high=input("here: ")
+    # img=document.getElementsByClassName("output_subarea output_image").item(0).children[0].src
     looprun='''
     document.getElementsByTagName("colab-run-button")[1].click()
-    img=document.getElementsByClassName("output_subarea output_image").item(0).children[0].src
-    document.getElementsByClassName("view-lines").item(1).children[11].children[0].children[0].innerText={}
+    document.getElementsByClassName("view-lines").item(1).children[11].children[0].children[0].innerText="{}"
     '''.format("# "+high)
-    driver.execute_script(looprun)
-    # output_subarea output_image
-    if high=="l":
-        print(img)
+    # driver.execute_script(looprun)
+
+    driver.find_element_by_xpath("//colab-run-button/").click()
+    driver.find_element_by_xpath("(//div[@class='view-lines'])[2]/div[11]/span/span").get_attribute("innerText")='# '+high
 
 
 
+    # WebDriverWait(driver, 7).until(not EC.presence_of_element_located((By.CLASS_NAME, 'cell-execution focused animating running')))
+    # cell-execution focused animating running
 
 
 
+    # document.getElementsByClassName('output_subarea output_image').item(0).children[0].src #only if clicked within iframe
+    iframe=driver.find_element_by_xpath("//div[@class='outputview']/iframe")
+    driver.switch_to.frame(iframe)
+
+    wait = WebDriverWait(driver, 1)
+    # WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,"//iframe[@id='textarea-WYSIWYG_ifr']")))
+    # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//body[@id='tinymce' and @class='mce-content-body']/p"))).send_keys("anynaynanya")
+
+
+    img = driver.find_element_by_xpath("//div[@class='output_subarea output_image']/img")
+    img = img.get_attribute("src")
+    print(img)
+
+    # driver.switch_to.default_content()
+    # print("nope",sys.exc_info())
+
+
+    # img=driver.execute_script("return document.getElementsByClassName('output_subarea output_image').item(0).children[0].src[get_img.index(':')+1:]")
+    # get_img = document.getElementsByClassName('output_subarea output_image').item(0).children[0].src
+    # get_img = get_img[get_img.index(':')+1:] # this will strip off 'javascript:' from the string name, so now 'func_name' contains 'onEditNumber();'
+    # img = driver.execute_script('return ' + get_img)
+
+    # print(img)
 
 
 
-
-
-
-# driver.quit()
-
-
-
-# time.sleep(5) # Let the user actually see something!
 # search_box = driver.find_element_by_name('q')
 # search_box.send_keys('ChromeDriver')
 # search_box.submit()
-# time.sleep(5) # Let the user actually see something!
 # driver.quit()
